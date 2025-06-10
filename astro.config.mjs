1503b4502b23d1c34ae2d7c78de8ed9ad2e6aeb0
Light <lightman.chang@gmail.com>
2025-06-10 10:03:33 +0800

    chore: 清空原 astro 專案結構
    將改用 Docusaurus 架構重建網站內容。

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-export default defineConfig({
-  site: 'https://www.weiqi.kids',
-  base: '/',
-  integrations: [tailwind()],
-});
